47472b09b175e105facc1fd99ef463a4de8b6855
wjfatuan <wilson.forero@uan.eud.co>
2023-03-09 21:03:58 -0500

    Add items in the listview

--- app/src/main/java/com/example/simpsons0216/MainActivity.kt
+++ app/src/main/java/com/example/simpsons0216/MainActivity.kt
@@ -12,45 +12,61 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.simpsons0216.adapters.SimpsonsAdapter
 import com.example.simpsons0216.databinding.ActivityMainBinding
 
-data class SimpsonCharacter (
-    val name:String,
+data class SimpsonCharacter(
+    val name: String,
     val fullName: String,
     val description: String,
     val photo: Int
 ) : java.io.Serializable {
 
 }
+
 class MainActivity : AppCompatActivity() {
-    var binding:ActivityMainBinding? = null
-    val simpsonNames = arrayListOf<String>("Homer","Bart","Lisa")
+    var binding: ActivityMainBinding? = null
+    val simpsonNames = arrayListOf<String>("Homer", "Bart", "Lisa")
     val simpsonCharacters = arrayListOf<SimpsonCharacter>(
         SimpsonCharacter("Homer", "Homer Simpson", "homer is a man ...", R.drawable.homer),
         SimpsonCharacter("Bart", "Bartolomeo Simpson", "bart is a man ...", R.drawable.bart),
-        SimpsonCharacter("Marge", "Marjorie Jacqueline Simpson", "Marjorie Jacqueline \"Marge\" Simpson is a character in the American animated sitcom The Simpsons and part of the eponymous family. Voiced by Julie Kavner, she first appeared on television in The Tracey Ullman Show short \"Good Night\" on April 19, 1987.", R.drawable.marge)
+        SimpsonCharacter(
+            "Marge",
+            "Marjorie Jacqueline Simpson",
+            "Marjorie Jacqueline \"Marge\" Simpson is a character in the American animated sitcom The Simpsons and part of the eponymous family. Voiced by Julie Kavner, she first appeared on television in The Tracey Ullman Show short \"Good Night\" on April 19, 1987.",
+            R.drawable.marge
+        )
     )
-    val startActivityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-        if(result.resultCode== Activity.RESULT_OK) {
-            Toast.makeText(this, "Result: "+result.data?.getStringExtra("RESULT"), Toast.LENGTH_SHORT).show()
-        } else {
+    lateinit var adapter: SimpsonsAdapter
+    val startActivityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                Toast.makeText(
+                    this,
+                    "Result: " + result.data?.getStringExtra("RESULT"),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
 
+            }
         }
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
         // 1. create the adapter object
         //val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, simpsonNames)
-        val adapter = SimpsonsAdapter(this, R.layout.list_simpsons, simpsonCharacters)
+        adapter = SimpsonsAdapter(this, R.layout.list_simpsons, simpsonCharacters)
         // 2. connect the list with the adapter
         binding?.simpsonList?.adapter = adapter
         binding?.simpsonList?.setOnItemClickListener { parent, view, position, id ->
-            println("\n-parent: $parent" +
-                    "\n-view: $view" +
-                    "\n-position: $position" +
-                    "\n-id $id")
-            showCharacter(simpsonNames[position].lowercase(), position)
+            println(
+                "\n-parent: $parent" +
+                        "\n-view: $view" +
+                        "\n-position: $position" +
+                        "\n-id $id"
+            )
+            showCharacter(simpsonCharacters[position].name.lowercase(), position)
         }
+        binding?.btnAddCharacter?.setOnClickListener { addCharacter() }
     }
 
     fun simpsonsOnClick(view: View) {
@@ -67,8 +83,20 @@ class MainActivity : AppCompatActivity() {
         startActivityResult.launch(intentCharacter)
     }
 
-    fun changeImage(character:String) {
+    fun changeImage(character: String) {
         val image = resources.getIdentifier(character, "drawable", packageName)
         binding?.simpsonImage?.setImageResource(image)
+    }
+
+    fun addCharacter() {
+        simpsonCharacters.add(
+            SimpsonCharacter(
+                "Milhouse",
+                "Milhouse",
+                "milhouse is a man ...",
+                R.drawable.bart
+            ),
+        )
+        adapter.notifyDataSetChanged()
     }
 }
